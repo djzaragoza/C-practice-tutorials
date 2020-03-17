1c73e512fb5f725ddd9d75c24e8d0077c2952936
unknown <dj.zaragoza@icloud.com>
2020-03-17 14:36:39 -0700

    started C# dependencies and print Hello Geeks

---
+++ index.cs
@@ -0,0 +1,17 @@
+//C# program to print Hello Geeks
+using System;
+
+namespace HelloGeeksApp
+{
+    class HelloGeeksApp
+    {
+        // Main function
+        static void Main(string[] args)
+        {
+            // printing Hello Geeks
+            Console.WriteLine("Hello Geeks");
+
+            Console.ReadKey();
+        }
+    }
+}
